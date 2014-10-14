3e8e3737dda724857cea5e56f7106deb8f4c4415
Daniel Adler <dadler1@gwdg.de>
2014-10-14 13:17:30 +0200

    added nodejs test

---
+++ tests/nodejs/nodemailer/mail.js
@@ -0,0 +1,26 @@
+var nodemailer = require('nodemailer')
+// var smtpTransport = require('nodemailer-smtp-transport');
+
+var T_HOST = process.env.T_HOST
+var T_USER = process.env.T_USER
+var T_PASS = process.env.T_PASS
+var T_FROM = process.env.T_FROM
+var T_TO   = process.env.T_TO
+
+var options = {
+  host: T_HOST,
+  secure: true,
+  auth: {
+    user: T_USER,
+    pass: T_PASS
+  }
+}
+
+var transporter = nodemailer.createTransport( options ); // smptTransport(options) );
+transporter.sendMail({
+  from: T_FROM,
+  to:   T_TO,
+  subject: "smtp-signer test mail | NodeJS (mail.js)",
+  text: "This test mail should have been signed.\n"
+});
+
