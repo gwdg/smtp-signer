88fe637bae8dbd9ebe0d93aa89208fca4bb849e9
Daniel Adler <dadler1@gwdg.de>
2015-01-29 11:48:50 +0100

    added password arg

--- tests/gen-test-cert.sh
+++ tests/gen-test-cert.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 # generate a self-signed certificate for testing purposes
 
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
   cat <<EOF
-usage: $0 <email>
+usage: $0 <email> <password>
 
 EOF
   exit 1
 fi
 
 EMAIL=$1
+PASSWORD=$2
 
-openssl genrsa -out ${EMAIL}_key.pem 2048
+openssl genrsa -out ${EMAIL}_key.pem -passout "pass:${PASSWORD}" 2048
 openssl req    -new -key ${EMAIL}_key.pem -out ${EMAIL}_csr.pem -subj /emailAddress=${EMAIL}
 openssl x509   -req -days 365 -in ${EMAIL}_csr.pem -signkey ${EMAIL}_key.pem -out ${EMAIL}_cert.pem
 
