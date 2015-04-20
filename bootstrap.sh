c3daf13da9c8f41274ddcb884da87dd87b6fea17
Daniel Adler <dadler1@gwdg.de>
2015-04-20 08:47:48 +0200

    updates for OpenBSD

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-PERL_MODS="Crypt::SMIME Log::Log4perl"
+PERL_MODS="Crypt::SMIME Log::Log4perl Log::Dispatch::Syslog"
 case `uname` in
   Darwin)
     for m in ${PERL_MODS}; do
       sudo perl -MCPAN -e"install $m"
     done
+    ;;
+  OpenBSD)
+    pkg_add postfix-2.12.20140109
     ;;
   *)
     echo "FIXME: unsupported platform"
