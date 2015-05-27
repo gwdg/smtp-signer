530f16d51bdf362c726379a1b53f2bfc75485c6f
Daniel Adler <dadler1@gwdg.de>
2015-05-27 09:05:38 +0200

    bootstrap perl mods on OpenBSD

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
-PERL_MODS="Crypt::SMIME Log::Log4perl Log::Dispatch::Syslog"
 case `uname` in
-  Darwin|OpenBSD)
+  Darwin)
+    PERL_MODS="Crypt::SMIME Log::Log4perl Log::Dispatch::Syslog"
     for m in ${PERL_MODS}; do
       sudo PERL_MM_USE_DEFAULT=1 perl -MCPAN -e"install $m"
+    done
+    ;;
+  OpenBSD)
+    pkg_add p5-Log-Log4perl-1.40 p5-Log-Dispatch-2.41p0
+    curl -L https://cpanmin.us | perl - --sudo App::cpanminus
+    PERL_MODS="Crypt::SMIME"
+    for m in ${PERL_MODS}; do
+      sudo cpanm --notest ${PERL_MODS}
     done
     ;;
   *)
